91947d36938cbedc268b4defe8a12b590ab2902f
Nathan Standafer <nathan.standafer@gmail.com>
2023-11-28 20:30:09 -0500

    section2

---
+++ section2/src/inference/UNetInferenceAgent.py
@@ -0,0 +1,97 @@
+"""
+Contains class that runs inferencing
+"""
+import torch
+import numpy as np
+
+from networks.RecursiveUNet import UNet
+
+from utils.utils import med_reshape
+
+import torch.nn.functional as F
+
+class UNetInferenceAgent:
+    """
+    Stores model and parameters and some methods to handle inferencing
+    """
+    def __init__(self, parameter_file_path='', model=None, device="cpu", patch_size=64):
+
+        self.model = model
+        self.patch_size = patch_size
+        self.device = device
+
+        if model is None:
+            self.model = UNet(num_classes=3)
+
+        if parameter_file_path:
+            self.model.load_state_dict(torch.load(parameter_file_path, map_location=self.device))
+
+        self.model.to(device)
+
+    def single_volume_inference_unpadded(self, volume):
+        """
+        Runs inference on a single volume of arbitrary patch size,
+        padding it to the conformant size first
+
+        Arguments:
+            volume {Numpy array} -- 3D array representing the volume
+
+        Returns:
+            3D NumPy array with prediction mask
+        """
+        print("single_volume_inference_unpadded, shape: {}".format(volume.shape))
+        print("single_volume_inference_unpadded, max: {}".format(np.max(volume)))
+        return self.single_volume_inference(volume)
+
+    def single_volume_inference(self, volume):
+        """
+        Runs inference on a single volume of conformant patch size
+
+        Arguments:
+            volume {Numpy array} -- 3D array representing the volume
+
+        Returns:
+            3D NumPy array with prediction mask
+        """
+        self.model.eval()
+        
+        #print("volume.shape: ", volume.shape)   #(33, 64, 64)
+        # Assuming volume is a numpy array of shape [X,Y,Z] and we need to slice X axis
+        slices = []
+
+        return_value = np.zeros(volume.shape)
+
+        # TASK: Write code that will create mask for each slice across the X (0th) dimension. After 
+        # that, put all slices into a 3D Numpy array. You can verify if your method is 
+        # correct by running it on one of the volumes in your training set and comparing 
+        # with the label in 3D Slicer.
+        # <YOUR CODE HERE>
+        volume_reshaped = med_reshape(volume, (volume.shape[0], self.patch_size, self.patch_size))
+        return_value = np.zeros(volume_reshaped.shape)
+
+        for i in range(volume_reshaped.shape[0]):
+            slice_input = volume_reshaped[i]
+
+            #print("slice_input.shape: ", slice_input.shape)
+
+            slice_input_tensor = torch.from_numpy(slice_input).unsqueeze(0).unsqueeze(0).type(torch.cuda.FloatTensor)
+            #print("slice_input_tensor.shape: ", slice_input_tensor.shape)
+
+            slice_prediction = self.model(slice_input_tensor)
+            #print("slice_prediction.shape: ", slice_prediction.shape)
+
+            
+            #using the softmax produces image data that most matches the label data, but inversed.  The array * -1 + 1 operation fixes this.
+            slice_prediction_array = F.softmax(slice_prediction, dim=1).cpu().detach().numpy()[0][0]       
+            #print("inference image average: {}".format(np.mean(slice_prediction_array)))
+            slice_prediction_array = slice_prediction_array * -1 + 1
+
+            #now, change it into a binary mask containing either 1's or 0's
+            slice_prediction_array[slice_prediction_array > 0.5] = 1
+            slice_prediction_array[slice_prediction_array <= 0.5] = 0
+
+            #print("inference image average post softmax: {}".format(np.mean(slice_prediction_array)))
+
+            return_value[i] = slice_prediction_array
+
+        return return_value
---
+++ section2/src/run_ml_pipeline.py
@@ -0,0 +1,85 @@
+"""
+This file contains code that will kick off training and testing processes
+"""
+import os
+import json
+
+from experiments.UNetExperiment import UNetExperiment
+from data_prep.HippocampusDatasetLoader import LoadHippocampusData
+
+class Config:
+    """
+    Holds configuration parameters
+    """
+    def __init__(self):
+        self.name = "Basic_unet"
+        self.root_dir = r"../../section1/out"
+        self.n_epochs = 40
+        self.learning_rate = 0.0002
+        self.batch_size = 8
+        self.patch_size = 64
+        self.test_results_dir = "../out"
+
+if __name__ == "__main__":
+    # Get configuration
+
+    # TASK: Fill in parameters of the Config class and specify directory where the data is stored and 
+    # directory where results will go
+    c = Config()
+
+    # Load data
+    print("Loading data...")
+
+    # TASK: LoadHippocampusData is not complete. Go to the implementation and complete it. 
+    data = LoadHippocampusData(c.root_dir, y_shape = c.patch_size, z_shape = c.patch_size)
+
+
+    # Create test-train-val split
+    # In a real world scenario you would probably do multiple splits for 
+    # multi-fold training to improve your model quality
+
+    keys = range(len(data))
+
+    # Here, random permutation of keys array would be useful in case if we do something like 
+    # a k-fold training and combining the results. 
+
+    split = dict()
+
+    # TASK: create three keys in the dictionary: "train", "val" and "test". In each key, store
+    # the array with indices of training volumes to be used for training, validation 
+    # and testing respectively.
+    # <YOUR CODE GOES HERE>
+    split['train'] = list(range(0, int(len(keys)*0.8)))
+    split['val']   = list(range(int(len(keys)*0.8), int(len(keys)*0.9)))
+    split['test']  = list(range(int(len(keys)*0.9), int(len(keys))))
+
+
+    # Set up and run experiment
+    
+    # TASK: Class UNetExperiment has missing pieces. Go to the file and fill them in
+    exp = UNetExperiment(c, split, data)
+
+    # You could free up memory by deleting the dataset
+    # as it has been copied into loaders
+    # del dataset 
+
+    # run training
+    
+    #exp.run()
+
+    # prep and run testing
+
+    # TASK: Test method is not complete. Go to the method and complete it
+    #exp.load_model_parameters('../out/2023-11-15_2236_Basic_unet/model.pth')
+    #exp.load_model_parameters('../out/2023-11-22_0134_Basic_unet/model.pth')
+    #exp.load_model_parameters('../out/2023-11-22_0208_Basic_unet/model.pth')
+    exp.load_model_parameters('../out/2023-11-24_1407_Basic_unet/model.pth')
+    
+    
+    results_json = exp.run_test()
+
+    results_json["config"] = vars(c)
+
+    with open(os.path.join(exp.out_dir, "results.json"), 'w') as out_file:
+        json.dump(results_json, out_file, indent=2, separators=(',', ': '))
+
